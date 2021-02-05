f71344aeeb762810cbb728d1b53481429cbf5cdb
yesyb <nicaiwho@163.com>
2021-02-05 18:02:14 +0800

    test: practice remote module

--- render/index.js
+++ render/index.js
@@ -1,12 +1,16 @@
-const fs = require('fs');
 const btn = document.getElementById('btn');
-const food = document.getElementById('food');
-// const btn = this.document.querySelector('#btn')
-// const food = this.document.querySelector('#food')
+// 渲染进程中使用主进程中的模块方法时，可以使用Remote解决在渲染和主进程间的通讯
+const BrowserWindow = require('electron').remote.BrowserWindow;
+let newWin = null;
 window.onload = function (){
     btn.onclick = function (){
-        fs.readFile('food.txt',(err,data)=>{
-            food.innerHTML = data;
+        newWin = new BrowserWindow({
+            width:500,
+            height:500
+        })
+        newWin.loadFile('red.html');
+        newWin.on('close',()=>{
+            newWin = null;
         })
     }
 };
