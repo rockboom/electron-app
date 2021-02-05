776237cb2c66017a3d133c4233184478201048b4
yesyb <nicaiwho@163.com>
2021-02-05 18:37:01 +0800

    test:click right key to show menu

--- render/index.js
+++ render/index.js
@@ -1,16 +1,11 @@
-const btn = document.getElementById('btn');
-// 渲染进程中使用主进程中的模块方法时，可以使用Remote解决在渲染和主进程间的通讯
-const BrowserWindow = require('electron').remote.BrowserWindow;
-let newWin = null;
-window.onload = function (){
-    btn.onclick = function (){
-        newWin = new BrowserWindow({
-            width:500,
-            height:500
-        })
-        newWin.loadFile('red.html');
-        newWin.on('close',()=>{
-            newWin = null;
-        })
-    }
-};
+const {remote} = require('electron');
+const rightTemplate = [
+    {label:"粘贴"},
+    {label:"复制"},
+]
+
+const menu = remote.Menu.buildFromTemplate(rightTemplate); 
+window.addEventListener('contextmenu',(e)=>{
+    e.preventDefault();
+    menu.popup({window:remote.getCurrentWindow()});
+},false);
